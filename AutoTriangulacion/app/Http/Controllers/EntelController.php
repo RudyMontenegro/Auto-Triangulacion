e9eebb37ce9282f0b7c50e3d5261ff0f54efff5d
rolidoss <rudymontenegro4@gmail.com>
2021-10-12 17:43:23 -0400

    Controladores Creados

---
+++ AutoTriangulacion/app/Http/Controllers/EntelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\entel;
+use Illuminate\Http\Request;
+
+class EntelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\entel  $entel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(entel $entel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\entel  $entel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(entel $entel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\entel  $entel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, entel $entel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\entel  $entel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(entel $entel)
+    {
+        //
+    }
+}
