f1aa9dcf6a8e4e58838f404d967b0e6b34c98158
alex-rodrigo-64 <alexcalle64@gmail.com>
2021-10-15 17:53:36 -0400

    cambios

--- AutoTriangulacion/resources/views/viva/excel.blade.php
+++ AutoTriangulacion/resources/views/viva/excel.blade.php
@@ -1,15 +1,42 @@
 @extends('layouts.app', ['page' => __('Icons'), 'pageSlug' => 'icons'])
 
 @section('content')
+  <style>
+    .subir{
+      padding: 10px 50px;
+      background: #d752ff;
+      color:#fff;
+      border:0px solid #fff;
+  }
+   
+  .subir:hover{
+      color:#fff;
+      background: #d752ff;
+  }
+  </style>
+  <script>
+    function cambiar(){
+      var pdrs = document.getElementById('file-upload').files[0].name;
+      document.getElementById('info').innerHTML = pdrs;
+  }
+  
+  </script>
+  
 <div class="row justify-content-center">
-<div class="col-md-6">
+<div class="col-md-10">
     <div class="card">
       <div class="card-header">
         <h4 class="card-title text-center">Subir Archivo Excel</h4>
       </div>
       <div class="card-body">
         <div class="alert alert-info">
-          <span>This is a plain notification</span>
+          
+          <label for="file-upload" class="subir">
+            <i class="fas fa-cloud-upload-alt"></i> Subir archivo
+        </label>
+        <input id="file-upload" onchange='cambiar()' type="file" style='display: none;'/>
+        <div id="info"></div>
+        <input type="submit" value="Enviar"> 
         </div>
         <div class="alert alert-info">
           <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
