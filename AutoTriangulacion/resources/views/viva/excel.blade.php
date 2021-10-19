618286d11535375d501447ac12a8638dcf29ad59
alex-rodrigo-64 <alexcalle64@gmail.com>
2021-10-19 16:06:24 -0400

    table upload

--- AutoTriangulacion/resources/views/viva/excel.blade.php
+++ AutoTriangulacion/resources/views/viva/excel.blade.php
@@ -38,25 +38,48 @@
         <div id="info"></div>
         <input type="submit" value="Enviar"> 
         </div>
-        <div class="alert alert-info">
-          <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
-            <i class="tim-icons icon-simple-remove"></i>
-          </button>
-          <span>This is a notification with close button.</span>
-        </div>
-        <div class="alert alert-info alert-with-icon" data-notify="container">
-          <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
-            <i class="tim-icons icon-simple-remove"></i>
-          </button>
-          <span data-notify="icon" class="tim-icons icon-bell-55"></span>
-          <span data-notify="message">This is a notification with close button and icon.</span>
-        </div>
-        <div class="alert alert-info alert-with-icon" data-notify="container">
-          <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
-            <i class="tim-icons icon-simple-remove"></i>
-          </button>
-          <span data-notify="icon" class="tim-icons icon-bell-55"></span>
-          <span data-notify="message">This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style.'</span>
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+              <div class="card">
+                   <form action="{{url('viva/create')}}" method="POST">
+                      <table class="table table-striped" id="tabla">
+                          <thead style="background : rgb(225, 78, 202)">
+                              <tr>
+                                  <th class="text-center">#</th>
+                                  <th class="text-center">Archivo</th>
+                                  <th class="text-center">Eliminar</th>
+                              </tr>
+                          </thead> 
+                          <tbody id="tabla3">
+                              <span id="estadoBoton"></span>
+                              <tr id="columna-0">
+                                  <th>
+                                      <input class="form-control" name="numeroUsuario[]" id="numeroUsuario"  style="border-color: rgb(225, 78, 202)" onkeyup="existe()"  
+                                      list="codigo" >
+                                      <datalist id="codigo">
+                                      </datalist>
+                                      <span id="estadoCodigo"></span>
+                                      <span id="estadoCodigoI"></span>
+                                  </th>
+                                  <td>
+                                    <label for="file-upload" class="subir">
+                                      <i class="fas fa-cloud-upload-alt"></i> Subir archivo
+                                  </label>
+                                  <input id="file-upload" onchange='cambiar()' type="file" style='display: none;'/>
+                                  </td>
+                                  <td class="eliminar" id="deletRow" name="deletRow">
+                                      <button class="btn btn-icon btn-danger"  type="button">
+                                          <span class="btn-inner--icon"><i class="ni ni-fat-remove"></i></span>
+                                      </button>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+  
+                  <button type="button" class="btn btn-success btn-lg btn-block" id="adicional" name="adicional">Añadir</button>
+      </form>
+              </div>
+          </div>
         </div>
       </div>
     </div>
