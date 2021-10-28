ffd304aa286ef839c84575afb090671c8c2ade18
kaolio <49197208+kaolio@users.noreply.github.com>
2021-10-28 17:21:42 -0400

    cambios de tigo1

--- AutoTriangulacion/routes/web.php
+++ AutoTriangulacion/routes/web.php
@@ -59,7 +59,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 Route::group(['middleware' => 'auth'], function () {
 	
-	Route::post('entel/register/XLSX/view', ['as' => 'entel.subirExcel', 'uses' => 'TigoController@subirExcel']);
+	//Route::post('entel/register/XLSX/view', ['as' => 'entel.subirExcel', 'uses' => 'TigoController@subirExcel']);
 	
 	Route::get('tigo/register',['as' => 'tigo.create','uses' => 'TigoController@create']);
 	Route::post('tigo/register/XLSX',['as' => 'tigo.store','uses' => 'TigoController@store']);
